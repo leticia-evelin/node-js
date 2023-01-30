458f103ca8cfe4a7be226568c9bf495744286da1
LETÍCIA EVELIN AGUIAR E SILVA <22282118@CFP127ALUNO.LOCAL>
2023-01-30 11:28:47 -0300

    exercicio aula 3

--- aula03/app.js
+++ aula03/app.js
@@ -1,6 +1,88 @@
 /****************************************************** 
-* Objetivo: 
+* Objetivo: Exercício 1 - operações matemática
 * Data: 30/01/2023
 * Autor: Letícia Evelin
 * Versão: 1.0
 *******************************************************/
+
+console.log('Sistema de operações matemáticas!')
+
+const { notDeepEqual } = require('assert');
+const { captureRejectionSymbol } = require('events');
+const { stdin, stdout } = require('process');
+//Import da bilioteca para entrada de dados
+var readline = require('readline');
+const { Script } = require('vm');
+
+//Criamos um objeto para manipular a entrada de dados
+var entradaDeDados = readline.createInterface({
+    input : process.stdin,
+    output: process.stdout
+});
+
+ // Funções de CallBack para entrada da operação e dos valores
+entradaDeDados.question('Por favor, escolha a operação do seu cálculo: \n (+) adição \n (-) subtração \n (*) multiplicação \n (/) divisão \n', function(operacao){
+    let operacaoCalculo = operacao; 
+
+    entradaDeDados.question('Digite o primeiro número: \n', function(valor1){
+        let primeiroValor = parseFloat(valor1.replace(',' , '.'));
+        console.log(primeiroValor);
+
+        entradaDeDados.question('Digite o segundo número: \n', function(valor2){
+            let segundoValor = parseFloat(valor2.replace(',' , '.'));
+            console.log(segundoValor);
+
+            console.log(operacaoCalculo, valor1, valor2);
+    
+            let resultado = 0;
+
+
+
+            // Validações
+             if(valor1 == '' || valor2 == ''){
+                console.log('ERRO: É necessário digitar algum valor nas entradas');
+
+                // Validação para entrada de dados não numericos
+            }else if(isNaN(primeiroValor) || isNaN(segundoValor)){
+                console.log('ERRO: É necessário que todos os valores digitados sejam números.');
+
+            } else if(operacaoCalculo =! '+ - * /'){
+                console.log('ERRO: É necessário digitar uma das operações válidas');
+
+            }
+            
+            // Cálculo
+            if(operacao == '+'){
+                resultado = (Number (valor1) + Number (valor2));
+                console.log('Resultado de ' + (valor1) + (operacao) + (valor2) + ' = ' + resultado);
+
+            }else if(operacao == '-'){
+                resultado = (Number (valor1) - Number (valor2));
+                console.log('Resultado de ' + (valor1) + (operacao) + (valor2) + ' = ' + resultado);
+
+            }else if(operacao == '*'){
+                resultado = (Number (valor1) * Number (valor2));
+                console.log('Resultado de ' + (valor1) + (operacao) + (valor2) + ' = ' + resultado);
+
+            }else if(operacao == '/'){
+                resultado = (Number (valor1) / Number (valor2));
+                console.log('Resultado de ' + (valor1) + (operacao) + (valor2) + ' = ' + resultado);
+            }
+        });
+    });
+});
+
+
+
+    
+
+
+        
+
+        
+     
+
+
+    
+
+
