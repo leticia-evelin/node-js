63342479f2fbe6184dad4a597590eb2961dd4883
LETÍCIA EVELIN AGUIAR E SILVA <22282118@CFP127ALUNO.LOCAL>
2023-04-24 09:13:42 -0300

    prisma

---
+++ aula08/controller/controller_aluno.js
@@ -0,0 +1,31 @@
+/***************************************************************
+ * Objetivo: Implementar a regra de negócios entre o app e model
+ * Data: 24/04/2023
+ * Autor: Letícia Evelin
+ * Versão: 1.0
+ **************************************************************/
+
+//função para receber os dados do app e enviar para o model para inserir novo item
+const inserirAluno = function(dadosAluno){
+
+};
+
+//função para receber os dados do app e enviar para o model para atualizar um item existente
+const atualizarAluno = function(dadosAluno){
+
+};
+
+//função para excluir um aluno filtrado pelo id, será encaminhado para o model
+const deletarAluno = function(id){
+
+};
+
+//função para retornar todos os itens da tabela recebidos do model
+const selecionarTodosAlunos = function(){
+
+};
+
+//função para buscar um item filtrando pelo id, será enchaminhado para o model
+const buscarIdAluno = function(id){
+
+};
